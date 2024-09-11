78043ee995208d46ae86e022d41ee8fd03f2cb86
TimashaWanninyake <160386834+TimashaWanninyake@users.noreply.github.com>
2024-09-11 09:06:01 +0530

    cc

--- main.js
+++ main.js
@@ -3,49 +3,48 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
 
-    if (n > slides.length) {slide
-        Index = 1
-    }
+  if (n > slides.length) {
+    slide;
+    Index = 1;
+  }
 
-    if (n < 1) {
-        slideIndex = slides.length
-    }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
-
-
 
 function clearFields() {
   // Clear the input fields
-  document.getElementById('name').value = '';
-  document.getElementById('donation').value = '';
-  document.getElementById('phone').value = '';
-  document.getElementById('email').value = '';
-  document.getElementById('address').value = '';
+  document.getElementById("name").value = "";
+  document.getElementById("donation").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("address").value = "";
 
   // Submit the form
-  document.getElementById('myForm').submit();
+  document.getElementById("myForm").submit();
 }
